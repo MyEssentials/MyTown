e4070a3449f2c75ed9d80e960d43bed2e85c929c
Joe Goett <jgoett154@gmail.com>
2013-11-14 19:35:32 -0500

    Fixed java.lang.AbstractMethodError

--- src/ee/lutsu/alpha/mc/mytown/entities/PayHandler.java
+++ src/ee/lutsu/alpha/mc/mytown/entities/PayHandler.java
@@ -38,7 +38,7 @@ public class PayHandler {
             return false;
         }
         
-        if (ForgePerms.getEconomyManager().withdraw(owner.name(), owner.onlinePlayer.worldObj.provider.getDimensionName(), requestedItem.itemID + ":" + requestedItem.getItemDamage(), requestedItem.stackSize)){
+        if (ForgePerms.getEconomyManager().playerWithdraw(owner.name(), owner.onlinePlayer.worldObj.provider.getDimensionName(), requestedItem.itemID + ":" + requestedItem.getItemDamage(), requestedItem.stackSize)){
             purchaseComplete();
             return true;
         }
@@ -64,7 +64,7 @@ public class PayHandler {
                 timeUntil = System.currentTimeMillis() + timeToPaySec * 1000;
                 notifyUser();
             } else{
-                if (ForgePerms.getEconomyManager().withdraw(owner.name(), owner.onlinePlayer.worldObj.provider.getDimensionName(), requestedItem.itemID + ":" + requestedItem.getItemDamage(), requestedItem.stackSize)){
+                if (ForgePerms.getEconomyManager().playerWithdraw(owner.name(), owner.onlinePlayer.worldObj.provider.getDimensionName(), requestedItem.itemID + ":" + requestedItem.getItemDamage(), requestedItem.stackSize)){
                 	MyTown.sendChatToPlayer(owner.onlinePlayer, "Took " + ForgePerms.getEconomyManager().format(requestedItem.itemID + ":" + requestedItem.getItemDamage(), requestedItem.stackSize));
                     purchaseComplete();
                 }
