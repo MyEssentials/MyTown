7ac5cd5fcf89fac4a992c50cb08dcdfe93734443
Joe Goett <jgoett154@gmail.com>
2013-12-14 23:55:35 -0500

    Added support for BlueSlimes to be despawned from towns if mobs are off.

--- src/ee/lutsu/alpha/mc/mytown/event/prot/TinkersConstruct.java
+++ src/ee/lutsu/alpha/mc/mytown/event/prot/TinkersConstruct.java
@@ -1,22 +1,30 @@
 package ee.lutsu.alpha.mc.mytown.event.prot;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
+import ee.lutsu.alpha.mc.mytown.ChunkCoord;
+import ee.lutsu.alpha.mc.mytown.MyTown;
+import ee.lutsu.alpha.mc.mytown.MyTownDatasource;
 import ee.lutsu.alpha.mc.mytown.Utils;
 import ee.lutsu.alpha.mc.mytown.entities.Resident;
+import ee.lutsu.alpha.mc.mytown.entities.TownBlock;
 import ee.lutsu.alpha.mc.mytown.entities.TownSettingCollection.Permissions;
 import ee.lutsu.alpha.mc.mytown.event.ProtBase;
+import ee.lutsu.alpha.mc.mytown.event.ProtectionEvents;
 
 public class TinkersConstruct extends ProtBase {
     public static TinkersConstruct instance = new TinkersConstruct();
 
-    private Class<?> clHammer, clExcavator;
+    private Class<?> clHammer, clExcavator, clBlueSlime;
 
     @Override
     public void load() throws Exception {
         clHammer = Class.forName("tconstruct.items.tools.Hammer");
         clExcavator = Class.forName("tconstruct.items.tools.Excavator");
+        clBlueSlime = Class.forName("tconstruct.entity.BlueSlime");
     }
 
     /**
@@ -44,10 +52,46 @@ public class TinkersConstruct extends ProtBase {
         }
         return null;
     }
+    
+    @Override
+    public String update(Entity e){
+    	if (clBlueSlime.isInstance(e)){
+    		EntityLiving mob = (EntityLiving) e;
+            if (e.isEntityAlive()) {
+                if (!canBe(mob.dimension, mob.posX, mob.posY, mob.posY +1, mob.posZ)) {
+                    // silent removal of the mob
+                    ProtectionEvents.instance.toRemove.add(e);
+                    return null;
+                }
+            }
+    	}
+    	
+    	return null;
+    }
+
+    private boolean canBe(int dim, double x, double yFrom, double yTo, double z) {
+        TownBlock b = MyTownDatasource.instance.getBlock(dim, ChunkCoord.getCoord(x), ChunkCoord.getCoord(z));
+        if (b != null && b.settings.yCheckOn) {
+            if (yTo < b.settings.yCheckFrom || yFrom > b.settings.yCheckTo) {
+                b = b.getFirstFullSidingClockwise(b.town());
+            }
+        }
+
+        if (b == null || b.town() == null) {
+            return !MyTown.instance.getWorldWildSettings(dim).disableMobs;
+        }
+
+        return !b.settings.disableMobs;
+    }
 
     @Override
     public boolean isEntityInstance(Item e) {
         return clHammer.isInstance(e) || clExcavator.isInstance(e);
+    }
+    
+    @Override
+    public boolean isEntityInstance(Entity e){
+    	return clBlueSlime.isInstance(e);
     }
 
     @Override
