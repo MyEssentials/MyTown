9ad2dc61b5b23442bba8555d9ecd2f3f7172be45
Joe Goett <jgoett154@gmail.com>
2013-12-15 13:26:54 -0500

    Changed the location of the IExplosiveCart.java file

---
+++ src/mods/railcraft/api/carts/IExplosiveCart.java
@@ -0,0 +1,17 @@
+package mods.railcraft.api.carts;
+
+public abstract interface IExplosiveCart{
+  public abstract void setPrimed(boolean paramBoolean);
+
+  public abstract boolean isPrimed();
+
+  public abstract int getFuse();
+
+  public abstract void setFuse(int paramInt);
+
+  public abstract float getBlastRadius();
+
+  public abstract void setBlastRadius(float paramFloat);
+
+  public abstract void explode();
+}
